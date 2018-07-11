2c8630a31b9048c260a0df56e765856000112b18
Vinícius Batista Barreira <vinicius.barreira@icomunicacao.com.br>
2018-07-11 13:51:10 -0300

    Treinamento intermediario de java script

---
+++ PHP do Zero ao Profissional/Módulo 7 - Java Intermediário/main.js
@@ -0,0 +1,33 @@
+function apertouMouse() {
+    console.log("Apertou o mouse");
+}
+
+function tirouMouse() {
+    console.log("Soltou o mouse");
+}
+
+function mouseencima() {
+    console.log("passou o mouse");
+}
+
+function clicou() {
+    console.log('Clicou!');
+}
+
+function onleft() {
+    console.log('botao direito!');
+    return false;
+}
+
+function cliqueduplo()
+{
+    console.log('Clicou Duas!');
+}
+
+function apertouTecla(event){
+    console.log('Apertou Tecla: ' +event.keyCode);
+}
+
+function carregou(){
+    console.log('carregou');
+}
