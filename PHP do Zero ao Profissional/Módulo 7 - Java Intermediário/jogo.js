50d2a0d369fca40fada0f6509a6955fa03216fcb
Vinícius Batista Barreira <vinicius.barreira@icomunicacao.com.br>
2018-07-13 13:56:04 -0300

    Jogo da estourar bola

--- PHP do Zero ao Profissional/Módulo 7 - Java Intermediário/jogo.js
+++ PHP do Zero ao Profissional/Módulo 7 - Java Intermediário/jogo.js
@@ -1,1 +1,25 @@
-//Jogo.sj
+//Jogo.js//
+
+var dias =  ["Domingo","Sexta"];
+
+function addBola() {
+    var bola = document.createElement("div");
+    bola.setAttribute("class", "bola");
+
+    var p1 = Math.floor(Math.random() * 500);
+    var p2 = Math.floor(Math.random() * 400);
+
+    bola.setAttribute("style", "left:" + p1 + "px; top:" + p2 + "px;");
+
+    bola.setAttribute("onclick", "estourar(this)");
+
+    document.body.appendChild(bola);
+}
+
+function estourar(elemento) {
+    document.body.removeChild(elemento);
+}
+
+function iniciar() {
+    setInterval(addBola, 2000);
+}
