e1971e5695cfad4a0e02d849fe6783cf0c2787f2
Vinícius Batista Barreira <vinicius.barreira@icomunicacao.com.br>
2018-08-01 13:59:29 -0300

    Aulas jQuery

---
+++ PHP do Zero ao Profissional/Módlulo 14 - jQuery/js/script.js
@@ -0,0 +1,22 @@
+// $(document).ready(function () {
+//     alert("Carregou");
+// });
+
+// $(function () {
+//     alert("Carregou novamente");
+// })
+
+// function tudoPronto() {
+//     alert("Tudo Pronto");
+// }
+// $(document).ready(tudoPronto);
+
+// // Evitando conflitos
+// var $vinicius = jQuery.noConflict();
+// $vinicius().ready(function () {
+//     alert("Sem conflito");
+// });
+
+// jQuery(document).ready(function () {
+//     alert("Sem conflito novamente");
+// });
